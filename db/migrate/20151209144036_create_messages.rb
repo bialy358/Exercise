73138885adba5c6b756e9cbcf73664e3b54ce071
Bialy <bialy358@gmail.com>
2015-12-09 16:13:25 +0100

    Add messages model

---
+++ db/migrate/20151209144036_create_messages.rb
@@ -0,0 +1,13 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.text :title
+      t.text :content
+      t.string :reciver
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+    add_index :messages, [:user_id, :created_at]
+  end
+end
